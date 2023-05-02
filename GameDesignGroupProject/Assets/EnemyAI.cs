34b2d768b9701b0f1f0bf5e466344d29298e9c00
egormis <91596001+egormis@users.noreply.github.com>
2023-05-02 19:30:36 +0100

    Enemy Logic
    Enemy logic started

---
+++ GameDesignGroupProject/Assets/EnemyAI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask groundMask;
+    public LayerMask playerMask;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange;
+    public float attackRange;
+
+    public bool playerInSightRange; 
+    public bool playerInAttackRange;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patroling() {
+        if (!walkPointSet) SearchWalkPoint();
+    }
+
+    private void SearchWalkPoint() {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + rangeZ);
+        
+    }
+
+
+
+}
